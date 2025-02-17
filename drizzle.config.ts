a2a33368a8bb27fada127c5a72bab995f379bf29
Josef Hřebec <josefhrebec02@gmail.com>
2025-02-17 21:37:29 +0100

    feat: add drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
-import { env } from "bun";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "mysql",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
