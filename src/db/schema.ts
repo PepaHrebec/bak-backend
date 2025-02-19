614c6075c18a68c371cf1740bee847e916ed7027
Josef Hřebec <josefhrebec02@gmail.com>
2025-02-19 21:59:00 +0100

    feat: add favorites table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,15 @@
-import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
 
 export const usersTable = mysqlTable("users_table", {
-  id: serial().primaryKey(),
+  id: int().autoincrement().notNull().primaryKey(),
   name: varchar({ length: 255 }).notNull().unique(),
   password: varchar({ length: 255 }).notNull(),
+});
+
+export const repeatedWordsTable = mysqlTable("repeated_words_table", {
+  id: serial().primaryKey(),
+  userId: int()
+    .references(() => usersTable.id)
+    .notNull(),
+  word: varchar({ length: 255 }).notNull(),
 });
