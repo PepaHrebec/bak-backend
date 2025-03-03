3a1e7f6e87fb1a3c43b18c61277fc9b67c151f20
Josef Hřebec <josefhrebec02@gmail.com>
2025-03-03 16:08:09 +0100

    fix: url log

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,10 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import { migrate } from "drizzle-orm/mysql2/migrator";
 
+console.log("Hey");
+console.log(process.env.MYSQL_URL);
+console.log(process.env.DATABASE_URL);
+
 // docker.for.mac.localhost
 export const db = drizzle(
   process.env.NODE_ENV === "production"
